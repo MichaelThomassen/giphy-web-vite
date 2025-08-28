771e093823bdcc747e21e6df3f458f74d4fc7004
MichaelThomassen <michael@voelit.dk>
2025-08-28 16:09:34 +0200

    Correct placcement of overlay Text

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -3,9 +3,11 @@ type Props = { src: string; alt: string; text: string; pos: Pos };
 
 export function ImageCard({ src, alt, text, pos }: Props) {
   return (
-    <div className="card">
-      <img src={src} alt={alt} loading="lazy" />
-      {pos !== "below" && text && <div className={`overlay ${pos === "top" ? "at-top" : "at-bottom"}`}>{text}</div>}
+    <div>
+      <div className="card">
+        <img src={src} alt={alt} loading="lazy" />
+        {pos !== "below" && text && <div className={`overlay ${pos === "top" ? "at-top" : "at-bottom"}`}>{text}</div>}
+      </div>
       {pos === "below" && text && <div className="below">{text}</div>}
     </div>
   );
